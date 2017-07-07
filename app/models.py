f0a1b4e31589872902413601b2bd130df202b366
Ramakrishnan <Karthik_Ramakrishnan@gallup.com>
2017-07-07 16:54:48 -0500

    Added database support for followers

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from app import db
 from hashlib import md5
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nickname = db.Column(db.String(64), index=True, unique=True)
@@ -8,6 +13,12 @@ class User(db.Model):
     posts = db.relationship('Posts', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime)
+    followed = db.relationship('User',
+                               secondary=followers,
+                               primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin=(followers.c.followed_id == id),
+                               backref=db.backref('followers', lazy='dynamic'),
+                               lazy='dynamic')
 
     @property
     def is_authenticated(self):
