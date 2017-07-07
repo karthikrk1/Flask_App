1eca14c2c1cb9ecf89ce34faba9213e3afb56a5d
Ramakrishnan <Karthik_Ramakrishnan@gallup.com>
2017-07-07 15:52:17 -0500

    Added error handling code

--- config.py
+++ config.py
@@ -14,3 +14,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+# mail server setting
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# admin list
+ADMINS = ['you@example.com']
