5f6de1ed12ce706b2c2588573507679c58515d4d
Ramakrishnan <Karthik_Ramakrishnan@gallup.com>
2017-07-20 10:30:29 -0500

    Added Email support. Need Testing

--- app/emails.py
+++ app/emails.py
@@ -1,13 +1,19 @@
 from flask_mail import Message
-from app import mail
+from app import mail, app
 from flask import render_template
 from config import ADMINS
+from .decorators import async
+
+@async
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    send_async_email(app, msg)
 
 
 def follower_notification(followed, follower):
@@ -18,3 +24,4 @@ def follower_notification(followed, follower):
                                user=followed, follower=follower),
                render_template("follower_email.html",
                                user=followed, follower=follower))
+
